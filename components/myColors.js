f112dcaad1f22b9b4fed8fdf7d629e32474f4d09
quan97it <33288275+quan97it@users.noreply.github.com>
2018-12-20 12:11:46 +0700

    Add files via upload

---
+++ components/myColors.js
@@ -0,0 +1,4 @@
+export const COLOR_FACEBOOK = '#3b5998'
+export const COLOR_BLUE_LIGHT = 'rgb(0,166,173)'
+export const COLOR_DARK_GRAY = 'rgb(54,54,54)'
+export const COLOR_PINK = 'rgb(245,168,154)'
